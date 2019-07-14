208c25791d8104cf60f2f04c068e497226f9647c
AdarshRajesh <adarshrajesh19@gmail.com>
2019-07-13 21:40:11 -0700

    Update model.py

--- Mask_RCNN-master/mrcnn/model.py
+++ Mask_RCNN-master/mrcnn/model.py
@@ -1965,7 +1965,7 @@ class MaskRCNN():
             nms_threshold=config.RPN_NMS_THRESHOLD,
             name="ROI",
             config=config)([rpn_class, rpn_bbox, anchors])
-
+	print(rpn_rois)
         if mode == "training":
             # Class ID mask to mark class IDs supported by the dataset the image
             # came from.
